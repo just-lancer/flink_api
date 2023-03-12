063127f0758e5f2364230df9422f22eddea8aacc
panqinggao <pan_qinggao@163.com>
2023-03-12 16:17:54 +0800

    .

---
+++ src/main/java/case_/operator/sink/C012_LocalFileSink.java
@@ -0,0 +1,17 @@
+package case_.operator.sink;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Author: shaco
+ * Date: 2023/3/12
+ * Desc: Sink算子，写入到本地文件
+ */
+public class C012_LocalFileSink {
+    public static void main(String[] args) {
+        // TOTO 1、创建流执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+    }
+}
